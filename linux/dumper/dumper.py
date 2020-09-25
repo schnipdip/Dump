d0a709886d68c82d7588993de2ad16c98f7e0fb8
Christopher Herzog <cjh30@pct.edu>
2020-09-24 22:29:50 -0400

    master

---
+++ linux/dumper/dumper.py
@@ -0,0 +1,19 @@
+#!/usr/bin python3
+
+# need to locate back up storage - monitor state
+# need to locate input storage - monitor state
+# when both exist, back up input storage to back up storage
+
+class backupStorage():
+    def find_backup(self):
+        return True
+    
+    def monitor_backup(self):
+        return True
+
+class inputStorage():   
+    def find_input(self):
+        return True
+    
+    def monitor_input(self):
+        return True
