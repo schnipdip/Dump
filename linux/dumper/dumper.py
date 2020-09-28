005e49954acd73b5f418d479409fd6c8b8180493
schnipdip <cjh30@pct.edu>
2020-09-28 19:43:20 -0400

    Added unmount

--- linux/dumper/dumper.py
+++ linux/dumper/dumper.py
@@ -41,11 +41,8 @@ def find_backup():
         usb_device = usb.util.get_string(device, device.iManufacturer)
         usb_device_vendorID = hex(device.idVendor)
 
-     #   print(usb_device_vendorID)
-
         device_list[usb_device] = usb_device_vendorID
 
-    #print(device_list)
     return device_list
     
 def verify_usb(usb_device_list, backup_device, input_device):
@@ -60,57 +57,66 @@ def verify_usb(usb_device_list, backup_device, input_device):
             input_usb_device = usb_device_list[usb]
 
     #returns hex(vendorID) of connected USB 
-    print(backup_usb_device)
     return backup_usb_device, input_usb_device
 
 def make_udev_rules(backup_vendorID, input_vendorID, dumper_loc):
     udev_file = open('/etc/udev/rules.d/10.autobackup.rules', 'w+')
     
-    write_str = """SUBSYSTEM="block", ACTION="add", ATTRS{idVendor}==""" + '''"''' + backup_vendorID + '''"''' +""" SYMLINK+="external%n" RUN+="/bin/python3 " """ + dumper_loc
+    write_str = """SUBSYSTEM="block", ACTION="add", ATTRS{idVendor}==""" + '''"''' + backup_vendorID + '''"''' +""" SYMLINK+="external%n" RUN+="/bin/python3 """ + dumper_loc + '''"''' 
 
     udev_file.write(str(write_str))
 
     udev_file.close()
 
 def mount_usb(dbl, dil, mbl, mil, backup_name, input_name):
-    check_path_backup = (mbl + backup_name)
-    check_path_input = (mil + input_name)
+    check_path_backup = (mbl + 'backup')
+    check_path_input = (mil + 'source')
 
     if os.path.exists(check_path_backup):
         pass
     else:
         #make dir for mount point
-        makedir_backup = ('mkdir ' + mbl + backup_name)
+        makedir_backup = ('mkdir ' + mbl + 'backup')
         os.system(makedir_backup)
 
     if os.path.exists(check_path_backup):
         #make mount point for backup usb
-        backup_command = ('sudo mount ' + dbl + ' ' + mbl + backup_name)
-        print(backup_command)
+        backup_command = ('sudo mount -t auto ' + dbl + ' ' + mbl + 'backup')
+        
         os.system(backup_command)
     
     if os.path.exists(check_path_input):
         pass
     else:
         #make dir for mount point
-        makedir_input = ('mkdir ' + mil + input_name)
+        makedir_input = ('mkdir ' + mil + 'source')
         os.system(makedir_input)
 
     if os.path.exists(check_path_input):
         #make mount point for input usb
-        input_command = ('sudo mount ' + dil + ' ' + mil + input_name)
+        input_command = ('sudo mount -t auto ' + dil + ' ' + mil + 'source')
         print(input_command)
         os.system(input_command)
 
 def run_autobackup(dev_backup_loc, dev_input_loc, mnt_backup_loc, mnt_input_loc, backup_name, input_name):
-    INPUT_SOURCE = (mnt_input_loc + input_name)
+    INPUT_SOURCE = (mnt_input_loc + 'source')
     INPUT_DEVICE = (dev_input_loc)
-    BACKUP_SOURCE = (mnt_backup_loc + backup_name)
+    BACKUP_SOURCE = (mnt_backup_loc + 'backup')
     BACKUP_DEVICE = (dev_backup_loc)
+    
+    command = ('''/usr/bin/rsync -a {0} {1} ''').format(INPUT_SOURCE, BACKUP_SOURCE)
 
-    command = ('''/usr/bin/rsync -auz '{0}' '{1}' && /bin/umount '{2}' && /bin/umount '{3}' '''.format(BACKUP_SOURCE, INPUT_SOURCE, INPUT_DEVICE, BACKUP_DEVICE))
+    result = subprocess.run(command, shell=True)
+    
+    print (result)
 
-    os.system(command)
+def unmount_drives():
+    print('unmounting drives')
+    command = ('''/usr/bin/umount /mnt/*''')
+
+    subprocess.run(command, shell=True)
+
+
 
 if __name__ == "__main__":
     #get backupdevice and input device configuration settings
@@ -131,3 +137,5 @@ if __name__ == "__main__":
     #run backup
     run_autobackup(dev_backup_loc, dev_input_loc, mnt_backup_loc, mnt_input_loc, backup_device, input_device)
 
+    #unmount 
+    unmount_drives()
